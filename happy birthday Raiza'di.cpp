fb7c4c8d79061dbfd8bdb63319e5cc07ad49c911
Karan-Kalyana <karankalyana.work@gmail.com>
2025-07-27 14:27:55 +0530

    Add files via upload

---
+++ happy birthday Raiza'di.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "\t\t**********************************************************************************************************" << endl;
+    cout << "\t\t**       #  #  ##  #### #### #    #        #### ### #### ##### #  # ###   ##  #    #                    **" << endl;                 
+    cout << "\t\t**       #  # #  # #  # #  #  #  #         #  #  #  #  #   #   #  # #  # #  #  #  #                     **" << endl;
+    cout << "\t\t**       #### #### #### ####   #           ####  #  ###    #   #### #  # ####   #                       **" << endl;
+    cout << "\t\t**       #  # #  # #    #     #            #  #  #  # #    #   #  # #  # #  #  #                        **" << endl;
+    cout << "\t\t**       #  # #  # #    #    #             #### ### #  #   #   #  # ###  #  # #                         **" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+    cout << "\t\t**          _.--._ _.--._                                              _.--._ _.--._                    **" << endl;
+    cout << "\t\t**         .$$$$$$s$$$$$$.                                            .$$$$$$s$$$$$$.                   **" << endl;
+    cout << "\t\t**          $$$sss$$sss$$$$                                           $$$sss$$sss$$$$                   **" << endl;
+    cout << "\t\t**          .$$$$$ss$$$$$$$                                           .$$$$$ss$$$$$$$                   **" << endl;
+    cout << "\t\t**           $$$$$ss$$$$$$             ^    ^          ^    ^          $$$$$ss$$$$$$                    **" << endl;
+    cout << "\t\t**            .$$$s$$$$$.             (*)  (*)        (*)  (*)          .$$$s$$$$$.                     **" << endl;
+    cout << "\t\t**              $$s$$$$                |    |          |    |             $$s$$$$                       **" << endl;
+    cout << "\t\t**              .$s$$.                |~|  |~|        |~|  |~|             .$s$$.                       **" << endl;
+    cout << "\t\t**                $$                 _| |__| |________| |__| |_              $$                         **" << endl;
+    cout << "\t\t**                :                 | | |  | |        | |  | | |              :                         **" << endl;
+    cout << "\t\t**                 :                |    H   A   P   P   Y     |              :                         **" << endl;  
+    cout << "\t\t**                 ;                |^^^^^^^^^^^^^^^^^^^^^^^^^^|               :                        **" << endl;
+    cout << "\t\t**                ;                 |  B  I  R  T  H  D  A  Y  |              ;                         **" << endl;
+    cout << "\t\t**               ;                __|--------------------------|__           ;                          **" << endl;
+    cout << "\t\t**              ;                |                                |         ;                           **" << endl;
+    cout << "\t\t**                               |         R  A  I  Z A           |                                     **" << endl;
+    cout << "\t\t**                               |         ************           |                                     **" << endl;
+    cout << "\t\t**                               ----------------------------------                                     **" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+    cout << "\t\t**********************************************************************************************************" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+    cout << "\t\t**                  /|\                                                                                  **" << endl;
+    cout << "\t\t**                 / * \                                                          /|\                     **" << endl;
+    cout << "\t\t**                -*|*|*-                   WISH YOU GOOD LUCK                   / * \                   **" << endl;
+    cout << "\t\t**               -**|*|**-                   WITH GOOD HEALTH                   -*|*|*-                 **" << endl;
+    cout << "\t\t**               _**|*|**_                     AND SUCCESS                     _**|*|**_                **" << endl;
+    cout << "\t\t**                -*|*|*-                                                       -*|*|*-                 **" << endl;
+    cout << "\t\t**                 \ * /                                                         \ * /                  **" << endl;
+    cout << "\t\t**                  \|/                                                           \|/                   **" << endl;
+    cout << "\t\t**                   #                                                             #                    **" << endl;
+    cout << "\t\t**     ###############         DEAR RAIZA DI,                                      ###############      **" << endl;
+    cout << "\t\t**      #@ ------- @#          THANKS FOR BEING MY BUILT-IN ELDER BROTHER,          #@ ------- @#       **" << endl;
+    cout << "\t\t**       #@ ----- @#           ALWAYS PROTECTING ME (AND BOSSING ME AROUND!)          #@ ----- @#       **" << endl;
+    cout << "\t\t**         *******             WITH LOTS OF LOVE FROM MUMMY,PAPA,RUBY'DI,KARAN.          ******         **" << endl;
+    cout << "\t\t**                                                                                                      **" << endl;
+	
+	        
+	
+	
+	return 0;
+}
